a34f511bc8cb47eaa13a5c0c944b9cf404354a37
TEHEK1 <kasapovamir9@gmail.com>
2024-02-29 14:08:20 +0300

    Improved ActionChanger::removeAction

--- include/ActionsChanger.h
+++ include/ActionsChanger.h
@@ -6,12 +6,14 @@
 #define CPPDUNGEON_ACTIONSCHANGER_H
 #include <memory>
 #include <set>
+#include <functional>
 class Player;
 class Action;
 class ActionsChanger{
 protected:
     void addAction(Player*, Action*);
-    void removeAction(Player*, bool (std::set<std::unique_ptr<Action>>::iterator));
+    void removeAction(Player*, std::function<bool (std::set<std::unique_ptr<Action>>::iterator)>);
     void returnToDefault(Player*);
+    std::set<std::unique_ptr<Action>>& getActions(Player*);
 };
 #endif //CPPDUNGEON_ACTIONSCHANGER_H
