26ed0b764a999cf28039c94540e87add1ddb62ad
Abdulla <kaituevabdulla@yandex.ru>
2024-03-03 20:38:29 +0300

    Use

---
+++ src/Use.cpp
@@ -0,0 +1,18 @@
+//
+// Created by kait on 3/3/24.
+//
+#include "Use.h"
+#include "Chest.h"
+
+Use::Use(Chest* chest, int index){
+    m_chest = chest;
+    m_index = index;
+}
+
+void Use::act(Player* player){
+    m_chest -> Chest::use(player, m_index);
+}
+
+Chest *Use::getChest() {
+    return m_chest;
+}
