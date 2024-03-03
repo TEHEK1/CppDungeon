d87718c6ed5eaebe66ad67db3a73f028a6faf8a8
Arseniy Borodulin <senya.borodulin@mail.ru>
2024-03-04 00:10:44 +0300

    fix: useless names

--- include/Chest.h
+++ include/Chest.h
@@ -12,7 +12,7 @@ class Chest:public Event, public InventoryChanger{
     bool comp(std::set<std::unique_ptr<Action>>::iterator actionIterator);
 public:
     void turn(Player *, int) override;
-    void use(Player *player, int index);
+    void use(Player *, int);
     std::vector<std::vector<char>> draw() override;
 };
 #endif //CPPDUNGEON_CHEST_H
