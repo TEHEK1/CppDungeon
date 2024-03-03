280f08b8786fc55f12e84d270292c3422ac6e6ea
Arseniy <71733778+J3olchara@users.noreply.github.com>
2024-03-04 01:03:41 +0300

    Task/event chroom emp cell (#2) (#32)
    added Event, ChooseRoomEvent, EmptyCell

---
+++ src/ChooseRoomEvent.cpp
@@ -0,0 +1,25 @@
+//
+// Created by Арсений Бородулин on 03.03.2024.
+//
+#include "ChooseRoomEvent.h"
+#include "ChooseNextRoom.h"
+#include "Map.h"
+#include "Monitor.h"
+#include "Player.h"
+
+
+void ChooseRoomEvent::turn(Player *player, int index) {
+    player->getMonitor()->draw();
+    player->getMap()->getCell(player->getPosition())->freeMoves(player, index);
+    for (std::pair<int, int> cords : player->getMap()->getNextRooms()) {
+        this->addAction(
+            player,
+            std::move(std::unique_ptr<ChooseNextRoom>(new ChooseNextRoom(cords)))
+        );
+    }
+}
+
+std::vector<std::vector<char>> ChooseRoomEvent::draw() {
+    std::vector<std::vector<char>> sprite;
+    return sprite;
+}
