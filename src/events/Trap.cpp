cee2d2a1c6b6e99918ff483ee5d31734fc0fe4d9
Abdulla <kaituevabdulla@yandex.ru>
2024-03-16 20:54:02 +0300

    12

--- src/events/Trap.cpp
+++ src/events/Trap.cpp
@@ -1,6 +1,18 @@
 //
 // Created by Амир Кашапов on 15.03.2024.
 //
-class Trap{
+#include "events/Trap.h"
+#include "player/Player.h"
+#include "events/Chest.h"
+#include "actions/Use.h"
+#include "navigation/Cell.h"
+#include "navigation/Map.h"
+#include "player/Item.h"
+#include <memory>
+#include "monitor/Monitor.h"
+#include "actions/Use.h"
 
-};
+void Trap::turn(Player * player) {
+    player -> getMonitor() -> draw();
+    addAction(player, this->getActions());
+}
