b40745e90453be315dbb79d61be6675bbe16627a
Abdulla <kaituevabdulla@yandex.ru>
2024-03-17 02:07:30 +0300

    Trap, got small bugs

--- include/events/Trap.h
+++ include/events/Trap.h
@@ -5,14 +5,17 @@
 #ifndef CPPDUNGEON_TRAP_H
 #define CPPDUNGEON_TRAP_H
 #include "events/UsableEvent.h"
+#include "changers/EffectChanger.h"
 class Player;
 namespace events {
-    class Trap : public events::UsableEvent {
+    class Trap : public events::UsableEvent, public changers::EffectChanger{
     public:
         void turn(Player *player) override;
         void use(Player *player) override;
         void dontUse(Player *player);
         std::vector<std::vector<char>> draw() override;
+    private:
+        bool m_isTrue = true;
     };
 } // namespace events
 #endif //CPPDUNGEON_TRAP_H
--- src/events/Trap.cpp
+++ src/events/Trap.cpp
@@ -2,7 +2,6 @@
 // Created by Амир Кашапов on 15.03.2024.
 //
 #include "generators/NumberGenerator.h"
-#include "effects/Debuff.h"
 #include "events/Trap.h"
 #include "player/Player.h"
 #include "events/Chest.h"
@@ -13,17 +12,35 @@
 #include "monitor/Monitor.h"
 #include "actions/Use.h"
 #include "actions/DontUse.h"
+#include "player/Squad.h"
+#include "effects/Damage.h"
 
 void events::Trap::turn(Player * player) {
     player -> getMonitor() -> draw();
     std::shared_ptr<events::UsableEvent> ptr_Use(dynamic_cast<events::UsableEvent*>(this));
-    addAction(player, std::shared_ptr<actions::Use>(new actions::Use(ptr_Use)));
+    if(m_isTrue) {
+        addAction(player, std::shared_ptr<actions::Use>(new actions::Use(ptr_Use)));
+    }
     addAction(player, std::shared_ptr<actions::DontUse>(new actions::DontUse(std::shared_ptr<Trap>(this))));
 }
 
 void events::Trap::dontUse(Player *player) {
-    int index = 1;
-    player -> getMap() -> getCell(player -> getPosition()) -> freeMoves(player, index);
+    static int random = generators::NumberGenerator::generate(0, 2);
+    std::vector<std::shared_ptr<entity::Entity>> entities = player -> getSquad() -> getEntities();
+    for(int i = 0; i < entities.size(); i++){
+        addEffect(entities[i], std::make_shared<effects::Damage>(random));
+    }
+    player -> getMap() -> getCell(player->getPosition())->freeMoves(player, std::shared_ptr<events::Event>(this));
+}
+
+void events::Trap::use(Player *player) {
+    static int random = generators::NumberGenerator::generate(0, 3);
+    std::vector<std::shared_ptr<entity::Entity>> entities = player -> getSquad() -> getEntities();
+    for(int i = 0; i < entities.size(); i++){
+        addEffect(entities[i], std::make_shared<effects::Damage>(random));
+    }
+    player -> getMap() -> getCell(player->getPosition())->freeMoves(player, std::shared_ptr<events::Event>(this));
+    m_isTrue = false;
 }
 
 std::vector<std::vector<char>> events::Trap::draw(){
