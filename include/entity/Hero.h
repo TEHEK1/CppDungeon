95f94a15ffc0e28b9b7c1aeb36aea2a6f2a7e402
TEHEK1 <kasapovamir9@gmail.com>
2024-03-18 11:15:10 +0300

    Added Hero template

---
+++ include/entity/Hero.h
@@ -0,0 +1,15 @@
+//
+// Created by Амир Кашапов on 18.03.2024.
+//
+
+#ifndef SKILLTEST_HERO_H
+#define SKILLTEST_HERO_H
+#include "entity/Entity.h"
+#include <string>
+#include <map>
+namespace entity{
+    class Hero: public entity::Entity{
+        Hero(std::string name, std::map<int, int> characteristics, std::set<std::shared_ptr<skillDesigns::Skill>> skills);
+    };
+}
+#endif //SKILLTEST_HERO_H
