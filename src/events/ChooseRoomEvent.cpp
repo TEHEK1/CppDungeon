d7ed76b4fec1bc92befda0c5f444ca65f3b054fb
TEHEK1 <kasapovamir9@gmail.com>
2024-03-18 12:59:56 +0300

    OK

--- src/events/ChooseRoomEvent.cpp
+++ src/events/ChooseRoomEvent.cpp
@@ -8,7 +8,7 @@
 #include "player/Player.h"
 
 
-//void events::ChooseRoomEvent::turn(Player *player) {
+void events::ChooseRoomEvent::turn(Player *player) {
 //    player->getMonitor()->draw();
 //    player->getMap()->getCell(player->getPosition())->freeMoves(player, std::shared_ptr<Event>(this));
 //    for (int coords : player->getMap()->getNextRooms()) {
@@ -17,7 +17,7 @@
 //            std::move(std::shared_ptr<actions::ChooseNextRoom>(new actions::ChooseNextRoom(coords)))
 //        );
 //    }
-//}
+}
 
 std::vector<std::vector<char>> events::ChooseRoomEvent::draw() {
     std::vector<std::vector<char>> sprite;
