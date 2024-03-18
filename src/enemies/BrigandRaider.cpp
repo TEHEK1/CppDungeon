6fac338c723a7d927380ad70dff68d89a8f976f0
TEHEK1 <67097507+TEHEK1@users.noreply.github.com>
2024-03-18 06:54:33 +0300

    Update BrigandRaider.cpp

--- src/enemies/BrigandRaider.cpp
+++ src/enemies/BrigandRaider.cpp
@@ -1,8 +1,9 @@
 #include "BrigandRaider.h"
 #include "EntityChanger.h"
 
-enemies::BrigandRaider::BrigandRaider(){
-    m_characteristics = {{static_cast<int>(Characteristic::accuracyModifier), 1}, 
+enemies::BrigandRaider::BrigandRaider():Enemy(
+    "BrigandRaider",
+    {{static_cast<int>(Characteristic::accuracyModifier), 1}, 
     {static_cast<int>(Characteristic::dodge), 26},
     {static_cast<int>(Characteristic::defence), 25},
     {static_cast<int>(Characteristic::speed), 5},
@@ -14,14 +15,12 @@ enemies::BrigandRaider::BrigandRaider(){
     {static_cast<int>(Characteristic::enemyType), static_cast<int>(EnemyType::Human)},
     {static_cast<int>(Characteristic::minDamage), 4},
     {static_cast<int>(Characteristic::maxDamage), 15},
-    {static_cast<int>(Characteristic::criticalDamageChance), 20}};
-    
-    m_resistances = {{static_cast<int>(Resistances::Stun), 73},
+    {static_cast<int>(Characteristic::criticalDamageChance), 20},
+    {static_cast<int>(Resistances::Stun), 73},
     {static_cast<int>(Resistances::Blight), 68},
     {static_cast<int>(Resistances::Bleed), 68},
     {static_cast<int>(Resistances::Debuff), 63},
-    {static_cast<int>(Resistances::Move), 73},};
-}
+    {static_cast<int>(Resistances::Move), 73}}){}
 
 std::vector<std::vector<char>> entity::Entity::drawAlive(){
     return {
