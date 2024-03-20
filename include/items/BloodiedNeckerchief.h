8715e9f3056b12d1f1b9081f325c75e1d05fdf1e
Abdulla <144126227+NAGIBATOR05@users.noreply.github.com>
2024-03-20 13:25:42 +0300

    Item (#63)
    * Use
    
    * make all Task Cell, Room, Hall
    
    * make all Task Cell, Room, Hall 2.0
    
    * fix random
    
    * fix random 2.0
    
    * MoveLeft and MoveRight
    
    * Update CMakeLists.txt
    
    * 12
    
    * save_dev
    
    * save_dev
    
    * proverka
    
    * save dev
    
    * save dev
    
    * Item
    
    * Item_2.0
    
    * Item
    
    * Item2.0
    
    * Update .gitignore
    
    * Item2.0
    
    * Item3.0
    
    * Reverted tests
    
    * Reverted tests
    
    * Fixed Items
    
    * Fixed

---
+++ include/items/BloodiedNeckerchief.h
@@ -0,0 +1,22 @@
+//
+// Created by kait on 3/19/24.
+//
+#ifndef UGABUGA_BLOODIEDNECKERCHIEF_H
+#define UGABUGA_BLOODIEDNECKERCHIEF_H
+#include "skillDesigns/Skill.h"
+#include "skillDesigns/RangeSkill.h"
+#include "items/Item.h"
+namespace items{
+    namespace BloodiedNeckerchief{
+        class BloodiedNeckerchief : Item{
+        public:
+            void unsafeTargetUse(int crited, std::shared_ptr<BattleField> battleField,
+                                 std::shared_ptr<entity::Entity> actor, std::shared_ptr<entity::Entity> object) override;
+            void unsafeSelfUse(int crited, std::shared_ptr<BattleField> battleField,
+                               std::shared_ptr<entity::Entity> object) override;
+            BloodiedNeckerchief();
+            std::string drawItem() override;
+        };
+    }// namespace BloodiedNeckerchief
+}//namespace items
+#endif //UGABUGA_BLOODIEDNECKERCHIEF_H
