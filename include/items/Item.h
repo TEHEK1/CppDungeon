0caa54d4c9d3abb49d0c233c993d53ec7977d35c
Abdulla <kaituevabdulla@yandex.ru>
2024-03-20 13:44:51 +0300

    asd

--- include/items/Item.h
+++ include/items/Item.h
@@ -5,10 +5,6 @@
 #ifndef CPPDUNGEON_ITEM_H
 #define CPPDUNGEON_ITEM_H
 #include "skillDesigns/RangeSkill.h"
-<<<<<<< HEAD
-namespace items {
-    class Item : public skillDesigns::RangeSkill {
-=======
 
 namespace items {
     class Item : public skillDesigns::RangeSkill {
@@ -18,7 +14,6 @@ namespace items {
                    std::vector<int> availableEnemyTarget = {},
                    int accuracy = 0, int criticalDamageChance = 0);
         virtual std::string drawItem() = 0;
->>>>>>> 8715e9f3056b12d1f1b9081f325c75e1d05fdf1e
     };
 }
 #endif //CPPDUNGEON_ITEM_H
